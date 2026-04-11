e79b3d146a92751422831765372dcaa9514c6278
Shreyas Mhade <mhadeshreyas911@gmail.com>
2026-04-11 12:49:21 +0530

    Updated home page responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './Home.css';
 import Navbar from '../Navbar/Navbar';
-import { FaDownload } from 'react-icons/fa'; './react-icons/fa'
+import MyResume from '../../assets/CV.pdf'
+import { FaCss3, FaDownload, FaHtml5, FaJava, FaJs, FaReact } from 'react-icons/fa'; './react-icons/fa'
 
 function Home() {
   return (
     <div className='home'>
       <Navbar/>
+      <div className='hero-section'>
       <div className='hero-content'>
         <h1>Shreyas Mhade</h1>
         <p>Full Stack Developer and B.Tech IT graduate with First Class distinction, passionate about building scalable web applications.</p>
         <div>
-            <button className='download-resume'><FaDownload/> Download Resume</button>
+            <a href={MyResume} download="My_Resume.pdf"><button className='download-resume'><FaDownload/> Download Resume</button></a>
         </div>
+      </div>
+      <div className='skill-content'>
+        <FaHtml5 />
+        <FaCss3 />
+        <FaJs />
+        <FaReact/>
+        <FaJava/>
+      </div>
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import myImage from '../../assets/my-pic.jpg';
+import { FaHamburger } from 'react-icons/fa';
 
 function Navbar() {
+  const [isOpen, setIsOpen] =useState(false);
+
+  const toggleMenu =() =>{
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav>
       <div className='portfolio-logo'>
         <img src={myImage} alt='Profile' />
         <h1>Portfolio</h1>
       </div>
+
+      <div className='hamburger' onClick={toggleMenu}>
+        <FaHamburger/>
+      </div>
       
-      <ul>
+      <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/projects">Projects</a></li>
