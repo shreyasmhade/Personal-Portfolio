2c8948d1b62859bf170125877809f05415b65bd0
Shreyas Mhade <mhadeshreyas911@gmail.com>
2026-04-14 13:48:30 +0530

    Created Projects Page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,43 +1,29 @@
 import React from 'react'
-import './Projects.css'
+import './projects.css'
 import Navbar from '../Navbar/Navbar'
-import PP from '../../assets/background-pic1.png'
 
 function Projects() {
-    const personalProjects=[
-        {
-            image:{PP},
-            name:"Personal Portfolio",
-            discription:"This is my personal portfolio website",
-            skills:["React.js","HTML","CSS"],
-            demo:"",
-            git:"https://github.com/shreyasmhade/Personal-Portfolio"
-        },
-        {
-            image:{PP},
-            name:"Personal Portfolio",
-            discription:"This is my personal portfolio website",
-            skills:["React.js","HTML","CSS"],
-            demo:"",
-            git:"https://github.com/shreyasmhade/Personal-Portfolio"
-        }
-    ]
+  const personalProject=[
+    {projectImage:{},
+     title:"Personal Portfolio",
+     discription:"",
+     skills:["",""],
+     demoLink:"",
+     githubLink:""
+     
+  },
+  ]
   return (
-    <div>
-      <Navbar/>
+    <div className='project'>
+      <Navbar />
       <div className='project-section'>
         <h1>Personal Projects</h1>
-      <div className='personal-projects'>
-        {personalProjects.map((project,index)=>(
-            <div key={index} className='project-card'>
-                {project.name}
-            </div>
-        ))}
-
-      </div>
-      <div className='assignment-projects'>
-        <h1>Assignment Projects</h1>
-      </div>
+        {personalProject.map((project,index)=>(
+          <div key={index} className='project-card'>
+            {project.name}
+          </div>
+        ))
+        }
       </div>
     </div>
   )
